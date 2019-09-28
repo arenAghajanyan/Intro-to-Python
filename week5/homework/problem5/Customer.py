60e0a8563506b281e725b92e6f329a32ebf8394d
arenAghajanyan <arenaghajan@gmail.com>
2019-09-28 08:34:34 +0400

    ops

--- week5/homework/problem5/Customer.py
+++ week5/homework/problem5/Customer.py
@@ -1,6 +1,6 @@
 import productcheck
 def buy(product, num, price):
-    if productcheck.check(product,num)==True:
+    if productcheck.check(product,num):
         print("You bought",product,"and spent",num*price)
     else:
         print("Sorry! We are out of this product.")
