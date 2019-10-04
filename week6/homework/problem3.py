486aefac1cb0c2562d4039555c6e900a2c900a55
arenAghajanyan <arenaghajan@gmail.com>
2019-10-04 15:17:00 +0400

    oo

--- week6/homework/problem3.py
+++ week6/homework/problem3.py
@@ -1,4 +1,11 @@
-import time
+import time 
+def dec1(fun):
+    def wr(*ar,**kar):
+        t1=time.time()
+        fun(*ar,**kar)
+        t2=time.time()
+        return ("It took",str((t2-t1)*1000),"miliseconds")
+    return wr
 class Person:
     def __init__(self,name,last_name,age,gender,student,password):
         self.name=name
@@ -7,35 +14,38 @@ class Person:
         self.gender=gender
         self.student=student
         self.__password=password
-    def dec1(fun):
-        def wr(*ar,**kar):
-            t1=time.time()
-            fun(*ar,**kar)
-            t2=time.time()
-            return t2-t1
-        return wr
+
     @dec1
     def Greeting(self,second_person):
-        return "Welcome dear",second_person.name
+        return "Welcome dear "+second_person.name
 
     def Goodbye(self):
         return "Bye everyone!"
+
     def Favourite_num(self, num1):
         try:
             if type(num1)!=int:
                 raise ValueError
             else:
-                return "My favourite number is",num1
+                return ("My favourite number is",num1)
         except ValueError:
             return "The input isn't a number"
+
     def Read_file(self,filename):
         try:
             open(filename+".txt")
         except Exception:
             print("No such file exists")
-a=Person("Lfcvbj","Lasdfg",123,"F",True,"asewqdzxc")
-b=Person("Lfycvj","Lasfyg",123,"yF",True,"asewyydc")
+
+    def setpassword(self,newpassword):
+        self.__password=newpassword
+
+    def getpassword(self):
+        return self.__password
+
+a=Person("Arron","sdfg",123,"F",True,"asewqdzxc")
+b=Person("Barron","Lasfyg",123,"yF",True,"wyydc")
 a.Read_file("fghj")
-a.Goodbye()
-print(a.Favourite_num("dffg"))
+print(a.Goodbye())
+print(a.Favourite_num('a'))
 print(a.Greeting(b))
